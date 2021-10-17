65d7eab4702fff00cf1d59a611f67bf93986126a
AfKsenia <afksenia@gmail.com>
2021-10-17 19:00:19 +0300

    Добавьте файлы проекта.

---
+++ 5_Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int m = 7;
+            double[] array = new double[m];
+            int i;
+            double S = 0;
+            for (i = 0; i < m; i++)
+            {
+                Console.WriteLine("Введите число {0}", i + 1);
+                array[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            for (i = 0; i < m; i++)
+            {
+                S += array[i];
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое элементов массива = {0:f2}", S / m);
+            Console.ReadKey();
+        }
+    }
+}
